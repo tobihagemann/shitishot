6c6bcceeca28fcfc52bca3f7785bfb441b9558cb
Tobias Hagemann <tobias@hagemann.org>
2018-02-03 20:11:01 +0100

    Added angular-l10n and ng-boostrap with basic localization structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,36 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+import { LocaleService, TranslationService, Language } from 'angular-l10n';
+
+interface Language {
+  text: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+
+  @Language() lang: string;
+  currentLanguage: Language;
+  languages: Language[] = [
+    { text: 'Deutsch', code: 'de' },
+    { text: 'English', code: 'en' }
+  ];
+
+  constructor(private titleService: Title, private localeService: LocaleService, private translationService: TranslationService) { }
+
+  ngOnInit() {
+    this.currentLanguage = this.languages.find(language => language.code == this.localeService.getCurrentLanguage());
+    this.translationService.translationChanged().subscribe(() => this.titleService.setTitle(this.translationService.translate('Title')));
+  }
+
+  selectLanguage(language: Language) {
+    this.currentLanguage = language;
+    this.localeService.setCurrentLanguage(language.code);
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { BrowserModule, Title } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { L10nConfig, L10nLoader, TranslationModule, StorageStrategy, ProviderType } from 'angular-l10n';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+const l10nConfig: L10nConfig = {
+  locale: {
+    languages: [
+      { code: 'en', dir: 'ltr' },
+      { code: 'de', dir: 'ltr' }
+    ],
+    language: 'en',
+    storage: StorageStrategy.Cookie
+  },
+  translation: {
+    providers: [
+      { type: ProviderType.Static, prefix: './assets/locale-' }
+    ],
+    caching: true,
+    missingValue: 'No key'
+  }
+};
+
+export function initL10n(l10nLoader: L10nLoader): Function {
+  return () => l10nLoader.load();
+}
 
 @NgModule({
+  providers: [
+    Title,
+    { provide: APP_INITIALIZER, useFactory: initL10n, deps: [L10nLoader], multi: true }
+  ],
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    TranslationModule.forRoot(l10nConfig),
+    NgbModule.forRoot(),
     AppRoutingModule
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private l10nLoader: L10nLoader) {
+    this.l10nLoader.load();
+  }
+
+}
