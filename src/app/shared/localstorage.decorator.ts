2bd1ba2b7b218006d738b084ed0b40e45c5e7549
Tobias Hagemann <tobias@hagemann.org>
2018-03-04 14:14:56 +0100

    Added key parameter to local storage decorator

--- src/app/shared/localstorage.decorator.ts
+++ src/app/shared/localstorage.decorator.ts
@@ -1,11 +1,13 @@
 /**
  * Property decorator for transparently retrieving/storing property from/in local storage when getter/setter is used.
  * @param defaultValue Getter returns `defaultValue` if local storage doesn't contain anything yet.
+ * @param key Key for local storage item. If null, it'll be the same as the property name.
  */
-export function LocalStorage(defaultValue = null) {
-  return function (target: any, key: string) {
-    const privateProperty = `_${key}`;
-    Object.defineProperty(target, key, {
+export function LocalStorage(defaultValue = null, key: string = null) {
+  return function (target: any, propertyName: string) {
+    key = key || propertyName;
+    const privateProperty = `_${propertyName}`;
+    Object.defineProperty(target, propertyName, {
       configurable: false,
       get: function () {
         if (this[privateProperty]) {
