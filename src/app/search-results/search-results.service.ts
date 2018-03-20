232f2b9a4fc656e8bc532bcbd1a6f9e264f579d2
Tobias Hagemann <tobias@hagemann.org>
2018-03-20 14:43:37 +0100

    Refactorings

--- src/app/search-results/search-results.service.ts
+++ src/app/search-results/search-results.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
-import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 import { BingService } from './bing.service';
 import { GoogleService } from './google.service';
@@ -20,28 +20,42 @@ export class SearchResultsService {
    * @param languageCode Language code.
    */
   getSearchResults(source: SearchResultsSource, title: string, languageCode: string): Observable<number> {
-    const beautify = map((value: number) => {
-      var count = 0;
-      while (value > 999) {
-        value = Math.floor(value / 10);
-        count++;
-      }
-      while (count > 0) {
-        value = value * 10;
-        count--;
-      }
-      return value;
-    });
     switch (source) {
       case SearchResultsSource.Yandex:
-        return this.yandexService.getSearchResults(title).pipe(beautify);
+        return this.yandexService.getSearchResults(title).map(value => this.beautifyNumber(value));
       case SearchResultsSource.Google:
-        return this.googleService.getSearchResults(title, languageCode).pipe(beautify);
+        return this.googleService.getSearchResults(title, languageCode).map(value => this.beautifyNumber(value));
       case SearchResultsSource.Bing:
-        return this.bingService.getSearchResults(title, languageCode).pipe(beautify);
+        return this.bingService.getSearchResults(title, languageCode).map(value => this.beautifyNumber(value));
       default:
-        return this.yandexService.getSearchResults(title).pipe(beautify);
+        return this.yandexService.getSearchResults(title).map(value => this.beautifyNumber(value));
     }
+  }
+
+  /**
+   * Returns a new number that retains at most the first three digits of the given number and replaces the rest with zero.
+   * 
+   * Examples:
+   * - 1 => 1
+   * - 12 => 12
+   * - 123 => 123
+   * - 1234 => 1230
+   * - 12345 => 12300
+   * - 123456 => 123000
+   * 
+   * @param value Number that will be beautified.
+   */
+  private beautifyNumber(value: number) {
+    var count = 0;
+    while (value > 999) {
+      value = Math.floor(value / 10);
+      count++;
+    }
+    while (count > 0) {
+      value = value * 10;
+      count--;
+    }
+    return value;
   }
 
 }
